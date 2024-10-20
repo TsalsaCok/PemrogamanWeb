ea64c2cb049f076e0b45c680c80969239fff5414
brontosaurus11 <Andis.fanany123@gmail.com>
2024-10-20 13:14:28 +0700

    modul2

--- modul2/Tugas 2/script.js
+++ modul2/Tugas 2/script.js
@@ -2,7 +2,6 @@ const todoInput = document.getElementById("todoInput");
 const addButton = document.getElementById("addButton");
 const todoList = document.getElementById("todoList");
 
-// Function to create a todo item
 function createTodoItem(task) {
   const todoItem = document.createElement("div");
   todoItem.className = "todo-item";
@@ -24,7 +23,7 @@ function createTodoItem(task) {
   todoList.appendChild(todoItem);
 }
 
-// Function to handle adding a new task
+
 const addTask = () => {
   const task = todoInput.value.trim();
   if (task) {
@@ -35,17 +34,14 @@ const addTask = () => {
   }
 };
 
-// Event listener for Add button
 addButton.onclick = addTask;
 
-// Event listener for Enter key press
 todoInput.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     addTask();
   }
 });
 
-// Function to edit an existing task
 function editTask(todoItem, taskText) {
   const newTask = prompt("Edit your task:", taskText.innerText);
   if (newTask !== null && newTask.trim() !== "") {
